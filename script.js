4d6024f17ae829f34527d3795668c7fb95f2487a
Sharu Nisha <166101755+sharunisha2003@users.noreply.github.com>
2024-07-23 16:38:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('accept').addEventListener('click', function() {
+    alert('You accepted the question!');
+});
+
+document.getElementById('decline').addEventListener('click', function() {
+    alert('You declined the question!');
+});
